c5baa4a0b91f6618a29c12b30b5d5f7dc0a3b901
panchaoheng1 <panchaoheng1@xiaomi.com>
2024-11-14 20:43:28 +0800

    ini

---
+++ my-hiui/src/views/Popover/index.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from "react"
+
+import "./index.scss"
+
+interface IProps {}
+
+const Popover: FC<IProps> = (props) => {
+  return (
+    <div className="popover">
+        
+    </div>
+  )
+}
+
+export default Popover
